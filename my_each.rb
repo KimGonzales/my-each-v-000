fadad76065896344bae3f774568599f1c46b5852
Kim Gonzales <kimgo589@gmail.com>
2018-01-24 22:25:40 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,6 @@
 def my_each(array)
   counter = 0
-   while counter < array.size
-     counter += 1
-     array.each do |i|
-       "This is the #{counter} time and we iterated over #{i}"
-     end
-   end
+  while counter < array.length
+    #iterate through this code block
+    counter +=1
 end
-
-
-my_each(["kim", "sheena", "randi"])
