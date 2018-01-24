36c8f2ce06f5e7e4979b35ca5b68990a0fc0344d
Kim Gonzales <kimgo589@gmail.com>
2018-01-24 22:27:50 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array)
   counter = 0
   while counter < array.length
-    #iterate through this code block and increment counter by 1
+    yield#iterate through this code block and increment counter by 1
     counter+=1
   end
 end
